6a78c9cabbb8b6976beff3f3fdc0728964ce439a
Wayne <age01282005@gmail.com>
2022-08-28 02:27:30 +0800

    add punch out eventbridge

--- event_bridge.tf
+++ event_bridge.tf
@@ -2,8 +2,7 @@
 resource "aws_cloudwatch_event_rule" "punch_in_event_rule" {
   name                = "auto-punch-in"
   description         = "Trigger random punch time at 10:00 +0800 "
-  //schedule_expression = "cron(0 0 10 ? * MON,TUE,WED,THU,FRI *)"
-  schedule_expression = "rate(5 minutes)"
+  schedule_expression = "cron(00 2 ? * MON-FRI *)"
 }
 
 resource "aws_cloudwatch_event_target" "punch_in_event_rule_target" {
@@ -11,11 +10,30 @@ resource "aws_cloudwatch_event_target" "punch_in_event_rule_target" {
   rule = aws_cloudwatch_event_rule.punch_in_event_rule.name
 }
 
-// Cloudwatch -----------------------------------------------------
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_rw_fallout_retry_step_deletion_lambda" {
-  statement_id  = "AllowExecutionFromCloudWatch"
+// Punch out -------------------------------------------------------------
+resource "aws_cloudwatch_event_rule" "punch_out_event_rule" {
+  name                = "auto-punch-out"
+  description         = "Trigger random punch time at 18:15 +0800 "
+  schedule_expression = "cron(15 10 ? * MON-FRI *)"
+}
+
+resource "aws_cloudwatch_event_target" "punch_out_event_rule_target" {
+  arn  = module.lambda_random_time.lambda_function_arn
+  rule = aws_cloudwatch_event_rule.punch_out_event_rule.name
+}
+
+resource "aws_lambda_permission" "allow_punch_in_cloudwatch_to_call_rw_fallout_retry_step_deletion_lambda" {
+  statement_id  = "AllowExecutionFromAutoPunchIn"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_random_time.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.punch_in_event_rule.arn
+}
+
+resource "aws_lambda_permission" "allow_punch_out_cloudwatch_to_call_rw_fallout_retry_step_deletion_lambda" {
+  statement_id  = "AllowExecutionFromAutoPunchOut"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_random_time.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.punch_out_event_rule.arn
 }
--- iam.tf
+++ iam.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role_policy_attachment" "iam_random_time_sqs" {
 }
 
 
-// sqs can trigger check-104 lambda
+// sqs can trigger auto-punch lambda
 resource "aws_iam_role_policy_attachment" "lambda_sqs_role_policy" {
   role       = module.lambda_auto_punch.lambda_role_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
@@ -37,7 +37,7 @@ resource "aws_iam_policy" "iam_sns_punch_status" {
 }
 
 
-// check-104 lambda can send msg to sns
+// auto-punch lambda can send msg to sns
 resource "aws_iam_role_policy_attachment" "iam_sns_punch_status" {
   role       = module.lambda_auto_punch.lambda_role_name
   policy_arn = aws_iam_policy.iam_sns_punch_status.arn
